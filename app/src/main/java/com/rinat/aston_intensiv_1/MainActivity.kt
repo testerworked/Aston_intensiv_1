76ea3689bb09170c25e7aef771f3a70a53f54972
Khuzin Rinat <r.khuzin@wrtech.ru>
2023-05-11 23:26:55 +0500

    add homework buttons

--- app/src/main/java/com/rinat/aston_intensiv_1/MainActivity.kt
+++ app/src/main/java/com/rinat/aston_intensiv_1/MainActivity.kt
@@ -2,10 +2,31 @@ package com.rinat.aston_intensiv_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.annotation.IdRes
 
 class MainActivity : AppCompatActivity() {
+
+    private val buttonFirst by bind<Button>(R.id.button1)
+    private val buttonSecond by bind<Button>(R.id.button2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.v("MainActivity", "Hello World");
+        Log.d("MainActivity", "Hello World");
+        Log.i("MainActivity", "Hello World");
+        Log.w("MainActivity", "Hello World");
+        Log.e("MainActivity", "Hello World");
+
+
     }
+
+    fun <T: Any?> bind(@IdRes idRes: Int): Lazy<T>{
+        return  lazy(LazyThreadSafetyMode.NONE) { findViewById<View>(idRes) as T }
+    }
+
 }
